ab279cb2b4773ec6a6777dedca4f6b1478d22dff
Luís Henrique <lhmaruyama@gmail.com>
2023-03-06 19:15:38 -0300

    feat: databse connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
 import express from "express";
+import {pool} from "./database"
+import { v4 as uuid4 } from "uuid"
 
 const app = express()
 app.use(express.json())
 
 import { calc } from "./suport"
 
-const a = 3
-const b = 2
+const a = 1
+const b = 1
 const c = calc(a,b)
-
 console.log(c);
-app.listen(4000,()=>{console.log("servidor rodando")})
+
+app.post("/user", (req: any, res: any) => {
+    const { name, email, password } = req.body
+    pool.getConnection((err: any, connection: any) => {
+        connection.query(
+            'INSERT INTO users (user_id, name, email, password) VALUES (?,?,?,?)',
+            [uuid4, name, email, password],
+            (error: any, result: any, fields: any) => {
+                if (error) {
+                    return res.status(400).json(error)
+                }
+                res.status(200).json({ success: true })
+
+            }
+
+        )
+    })
+})
+
+
+app.listen(4000,()=>{console.log("Servidor Rodando")})
